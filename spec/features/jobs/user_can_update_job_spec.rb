a3dcd7495bc8911df6707217584f7736137f65e8
Tyjoo26 <Tyjoo21@gmail.com>
2017-12-10 13:16:17 -0700

    Build out user can update job functionality with feature test, test passes

---
+++ spec/features/jobs/user_can_update_job_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "As a user" do
+  describe 'I see a form for updating a job' do
+    before :each do
+      @company = Company.create!(name: "ESPN")
+      @job = @company.jobs.create!(title: "Developer", description: "So hot right now...so hot", level_of_interest: 70, city: "Denver")
+
+      visit edit_company_job_path(@company, @job)
+    end
+
+    it 'has form fields and a submit button will redirect me to job index' do
+      fill_in "job[title]", with: "Developer"
+      fill_in "job[description]", with: "So fun!"
+      fill_in "job[level_of_interest]", with: 80
+      fill_in "job[city]", with: "Denver"
+
+      click_button "Submit"
+
+      expect(current_path).to eq("/companies/#{@company.id}/jobs/#{@job.id}")
+    end
+  end
+end
